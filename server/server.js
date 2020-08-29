c1b54311c38fa3369d139554ff38b514d998ac8c
Praful Gupta <prafulgupta6@gmail.com>
2020-08-30 00:22:35 +0530

    more pages + server

--- server/server.js
+++ server/server.js
@@ -5,30 +5,63 @@ const port = 4000
 
 var fs = require('fs');
 
+var multer = require('multer')
+var storage = multer.diskStorage(
+  {
+      destination: '../database/public/uploads/',
+      filename: function ( req, file, cb ) {
+          //req.body is empty...
+          //How could I get the new_file_name property sent from client here?
+          cb( null, file.originalname );
+      }
+  }
+);
+
+var upload = multer( { storage: storage } );
+
+app.post('/upload', upload.any(), function(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+  console.log(req.files)
+
+
+  res.send(req.files)
+
+})
+
 
 
 
 app.get('/template_count', (req, res) => {
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+
   console.log(req.body)
 
-
-  var reply = {
-      status : 200,
-      template : [
-          "template_1",
-          "template_2",
-          "template_3",
-          "template_4",
-          "template_5"
-      ]
+  const reply = {
+    templates :  [
+      "hello1",
+      "hello2"
+    ]
   }
-
 
   res.send(reply)
 
 
 })
+
+
+var cors = require('cors');
+app.use(cors({origin: 'http://localhost:3000'}));
+
 
 
 
